f98e65082d76f6e6acb687653b767831a33f60c5
André Valentin <ava@tradeshift.com>
2017-11-02 18:11:40 +0100

    Adding Google Analytics tag to the landing page.

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58044520-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-58044520-2');
+</script>
+
 <div class="container-fluid" style="padding:0;background: whitesmoke">
     <nav class="navbar navbar-default" style="padding-left: 15px;padding-right: 15px;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.50);">
         <div class="container-fluid">
